46ff9b535858af6dbaa768b41c349ba0176de483
leem26 <matt.lee94@berkeley.edu>
2020-08-03 20:49:37 -0700

    initial commit

---
+++ makedat.R
@@ -0,0 +1,11 @@
+#######################################################################
+##
+## Population Health Science End-of-Summer Camp
+## Some sample data to illustrate various concepts
+## Working title: "Follow-up of Über-cool New Students: The FÜN Study"
+## Author: Matthew Lee
+## Last updated: 
+   Sys.Date()
+##
+#######################################################################
+
